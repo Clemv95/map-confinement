7a25002ee6ee81e6a7bd8194abb67b1ce137ab9f
verhillcl <verhillcl@eisti.eu>
2020-03-28 18:57:20 +0100

    Click button while pressing enter

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
   <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
   <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
   <script src="https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/places.js@1.18.1"></script>
 
 </head>
 
@@ -23,7 +24,7 @@
     <div class="map-card__info">
       <p id='js-address'> </p>  
       <input type="text" value="" class="map-input" id="recherche" placeholder="Entrer une adresse"/>
-      <button type="button" class="btn" onclick="rayon()"> Go</button>
+      <button type="button" class="btn" id="btn" onclick="rayon()"> Go</button>
     </div>   
 
   </div>
@@ -31,7 +32,32 @@
 
 <script src="assets/js/map.js"></script>
 
+<script type="text/javascript">
+  var placesAutocomplete = places({
+    appId: 'plCZRAY9T7BG',
+    apiKey: '482b3fdbbbfcb03ab7227e5c2a3b8935',
+    container: document.querySelector('#recherche'),
+  });
+
+  var input = document.getElementById("recherche");
+
+  input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("btn").click();
+    }
+  });
+
+</script>
 
 
-
+<style type="text/css">
+  .ap-input-icon {
+    display: none;
+    visibility: hidden;
+  }
+  .ap-dropdown-menu{
+    width: 96%;
+  }
+</style>
 
